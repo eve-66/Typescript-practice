077734e2e34521ce46304691de301ae292dbba4e
eve-66 <is0733pi@ed.ritsumei.ac.jp>
2025-03-08 17:31:00 +0900

    React-turtorial

---
+++ src/button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function MyButton() {
+  return (
+    <div>
+        <button>I'M a Button</button>
+    </div>
+  )
+}
+
+export const YourButton = () => {
+  return (
+    <button>
+      You're a Button
+    </button>
+  )
+}
